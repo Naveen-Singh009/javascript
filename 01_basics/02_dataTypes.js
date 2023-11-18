efde2cb3d5d67f996be7b5a88052e344efe0002c
Naveen-Singh009 <naveengusaing@gmail.com>
2023-11-18 17:10:46 +0530

    Create 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all js code as newer js version
+
+//alert(2+4)  //we are using node js , not browser
+
+console.log(23
+    +
+    44
+    ) //code readability should be high so avoid these kind of coding
+
+
+console.log("naveen")
+
+let name = "hitesh"
+let age = 21
+let isLoggedIn = false
+let state = null
+//number  => 2^53 
+//bigInt => 
+//String => ""
+//boolean => true or false
+//null =>  stand alone value (this is actually al type)
+//undefined => value not assigned
+//symbol => unique 
+
+
+// Objects 
+
+console.log(typeof "naveen")
+console.log(typeof null)//object 
+console.log(typeof undefined)//undefined
+
+
+
