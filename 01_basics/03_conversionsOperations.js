0de544687d571135e80b56f36d577d4034ad1660
Naveen-Singh009 <naveengusaing@gmail.com>
2023-11-18 17:48:32 +0530

    conversionOfDatatypes

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueNumber = Number(score)
+
+console.log(typeof(valueNumber))
+
+console.log(valueNumber) //NaN not a number
+//"33" = 33
+//'2323ad" = NaN
+//true => 1 ; false => 0
+
+//null = 0
+//undefined = NaN
+//naveen = NaN
+
+let isLoggedIn = 1
+let booeanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booeanIsLoggedIn)
+
+//1 => true
+// "" => false;
+//"naveen" => true
+
