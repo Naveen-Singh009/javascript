d2f9d6f0fdbb061128234a511094618a4ad3ba86
Naveen-Singh009 <naveengusaing@gmail.com>
2023-11-19 23:25:56 +0530

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 344;
+
+const balance = new Number(322)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // it will give the 2 value ater point
+
+const num2 = 23.5342
+// console.log(num2.toPrecision(3));
+// it will give 23.5  total length if we give 2 here the value will be in exponential
+
+
+const hndreds  = 10000000
+// console.log(hndreds.toLocaleString());//this is for us number system
+// console.log(hndreds.toLocaleString('en-IN'));//this is for indian number system
+// it will give the output coma saperated 
+
+
+//+++++++++++++ Maths ++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-32));
+// console.log(Math.round(4.6)); //roundof
+// console.log(Math.ceil(4.2)); //top value
+// console.log(Math.floor(4.2)); //floor value
+// console.log(Math.min(3,42,5,2,1)); //min among these
+// console.log(Math.max(2,4,55,21)); //max among these
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+min)
