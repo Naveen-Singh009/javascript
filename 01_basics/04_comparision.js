2c372aafda30d156b8c6f850bd00d601eebfe0c9
Naveen-Singh009 <naveengusaing@gmail.com>
2023-11-19 12:44:37 +0530

    comparisons done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+// in these "2 and 02 is converted into integert"
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+
+
+//avoid these type of conversions
+
+// null is convrted into diff types like NaN , 0 etc 
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// undefined always false
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+
+
+// === "strict check " => value as well as datatype is checked
+
+console.log("2"==2)
+console.log("2" === 2)
