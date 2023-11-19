2225eec949319530340581f98e85f6d255118cfe
Naveen-Singh009 <naveengusaing@gmail.com>
2023-11-19 12:19:23 +0530

    operations

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -1,14 +1,14 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 let valueNumber = Number(score)
 
-console.log(typeof(valueNumber))
+// console.log(typeof(valueNumber))
 
-console.log(valueNumber) //NaN not a number
+// console.log(valueNumber) //NaN not a number
 //"33" = 33
 //'2323ad" = NaN
 //true => 1 ; false => 0
@@ -19,9 +19,57 @@ console.log(valueNumber) //NaN not a number
 
 let isLoggedIn = 1
 let booeanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booeanIsLoggedIn)
+// console.log(booeanIsLoggedIn)
 
 //1 => true
 // "" => false;
 //"naveen" => true
+
+
+// ****** operations 
+
+let value = 3
+let negVal = -value
+// console.log(negVal)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "naveen"
+let str2 = " Singh"
+
+let str3 = str1+str2
+console.log(str3)
+
+// console.log("1"+2)
+//if string first then all converted to string
+// console.log(1+"2") 
+//if number first all converted to number
+
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+let num1, num2 , num3
+num1 = num2 = num3 =23
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+
+
+
+
+
+
+
 
