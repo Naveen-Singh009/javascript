6d1c2a496e7f8badf578c4caf9f01a0f5b7c1225
Naveen-Singh009 <naveengusaing@gmail.com>
2023-11-20 17:32:23 +0530

    arrays in js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+//arrays
+
+const myArr = [0,1,2,3,4,5]
+
+const myH = ["shaktiman" , "nagraj"]
+
+const myArr2 = new Array(1,2,3,4,5)
+
+// console.log(myArr[0]);
+
+
+// myArr.push(6)
+// myArr.push(9)
+// myArr.pop()
+
+// myArr.unshift(9)  // this element is added to start so shifting takes place
+// myArr.shift()  // it will remove the shifted value (or the first value)
+
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(5)); // if not present then the result will be -1
+
+// const newArr = myArr.join() // binds the array and convert into string coma seperated
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+console.log("A " , myArr);
+const myn1 = myArr.slice(1,3) // this does manupulate original array //3 excluded
+
+console.log(myn1);
+console.log("B " , myArr);
+
+const myn2 = myArr.splice(1,3) // this method removes the part of array from the original array
+//mainupulate the original array
+console.log(myn2); // including 3
+
+console.log("c" , myArr);
