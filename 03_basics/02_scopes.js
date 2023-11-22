24f919e941ec1ae53e490fe48b3c30739f1150e0
Naveen-Singh009 <naveengusaing@gmail.com>
2023-11-22 11:25:07 +0530

    scoe

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+
+// var c = 32
+let a = 700
+if(true){
+    let a = 20 
+    const b = 33
+    var c = 90
+    console.log("inner" , a);
+}
+
+// console.log(a); not accessable
+// console.log(b); 
+console.log(c); // var is accessible
