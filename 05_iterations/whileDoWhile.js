94060e0be94c7289c431104e0a07664f50c24da1
Naveen-Singh009 <naveengusaing@gmail.com>
2023-11-23 16:10:59 +0530

    whileanddowhile

---
+++ 05_iterations/whileDoWhile.js
@@ -0,0 +1,27 @@
+// while loop
+
+// while(condition){
+
+// }
+
+
+let i = 0
+// while(i <= 10){
+//     console.log(`value of index is ${i}`);
+//     i++
+// }
+
+let myArr = ['flash' , 'batman' , 'superman']
+let arr = 0
+
+// while(arr < myArr.length){
+//     // console.log(`value is ${myArr[arr]}`);
+//     // arr = arr+1
+// }
+
+// do while loop 
+let score = 11
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score <=10);
